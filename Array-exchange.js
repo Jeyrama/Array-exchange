8174c398cba4fdde71f6d7d58e1fbf02c3f16a67
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-03-08 14:13:11 -0800

    Create Array-exchange.js

---
+++ Array-exchange.js
@@ -0,0 +1,13 @@
+/*
+It's time for some array exchange! The objective is simple -
+exchange the elements of two arrays in-place in a way that their new content is also reversed.
+
+Example:
+  const myArray = ['a', 'b', 'c'];
+  const otherArray = [1, 2, 3];
+
+  exchangeWith(myArray, otherArray);
+
+  myArray => [3, 2, 1]
+  otherArray => ['c', 'b', 'a']
+*/
